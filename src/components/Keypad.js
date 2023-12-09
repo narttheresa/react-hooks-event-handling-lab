ad36900faeba9cbc1f28d10165a11a419df5eeeb
Theresa Tran <theresa1194@gmail.com>
2023-12-09 14:17:35 +1100

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,21 @@
+import React from 'react';
+
+
 // Code Keypad Component Here
 
-function Keypad (){
+
+
+
+function Keypad() {
+
+    function handleChange() {
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange} />
+        </div>
     )
 }
 
